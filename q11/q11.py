6f326b468792e71cd1d4ddd87ea735fdb877eccb
Akashi_SN <btorntireinvynriy@gmail.com>
2016-11-14 16:13:08 +0900

    Update

--- q11/q11.py
+++ q11/q11.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
+import sys
 
 arr = [61,61,61,80,66,64,64,76,75,120,77,102,107,100,70,112,75,108,68,108,108,97,122]
-string = ""
-for i in range(1,20):
+for i in range(-61,61):
+	string =""
 	for ch in arr:
 		string += chr(ch+i)
-	print(string)
-	string = ""
-	for ch in arr:
-		string += chr(ch-i)
-	print(string)
-	string = ""
+	if "SECCON" in string:
+		print(string)
+		sys.exit()
